2c6f2343cc3b4fdad21373dcf42ced813934cf7e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-06-01 19:01:51 -0700

    create a way to massage the address data

--- server.js
+++ server.js
@@ -34,28 +34,28 @@ server.listen(process.env.PORT || 3000);
 //server.listen(3000);
 
 const randomAddresses = [
-    {1: "12343 NE Holladay Pl Portland, Oregon(OR), 97230"},
-    {2: "15564 SE Morrison St Portland, Oregon(OR), 97233"},
-    {3: "3125 SW 82nd Ave Portland, Oregon(OR), 97225"},
-    {4: "222 SW Columbia St Portland, Oregon(OR), 97201"},
-    {5: "2020 SW Broadway Dr #APT 7 Portland, Oregon(OR), 97201"},
-    {6: "1849 SE Ankeny St Portland, Oregon(OR), 97214"},
-    {7: "3417 N Michigan Ave Portland, Oregon(OR), 97227"},
-    {8: "1969 NW Johnson St #APT 302 Portland, Oregon(OR), 97209"},
-    {9: "7128 SW Burlingame Ave Portland, Oregon(OR), 97219"},
-    {10: "10339 NE Prescott St #306 Portland, Oregon(OR), 97220"},
-    {11: "10677 NW Lost Park Dr Portland, Oregon(OR), 97229"},
-    {12: "10145 SW Highland Dr Portland, Oregon(OR), 97224"},
-    {13: "15768 SE Powell Blvd #9 Portland, Oregon(OR), 97236"},
-    {14: "16035 SW King Charles Ave Portland, Oregon(OR), 97224"},
-    {15: "15920 NE Fremont St Portland, Oregon(OR), 97230"},
-    {16: "2536 SE Harrison St #9 Portland, Oregon(OR), 97222"},
-    {17: "236 SE 74th Ave Portland, Oregon(OR), 97215"},
-    {18: "12030 SW 97th Pl Portland, Oregon(OR), 97223"},
-    {19: "12614 E Burnside St Portland, Oregon(OR), 97233"},
-    {20: "200 SW Market St #1770 Portland, Oregon(OR), 97201"},
-    {21: "4926 SW 19th Ave Portland, Oregon(OR), 97201"},
-    {22: "5038 SE 30th Ave #32 Portland, Oregon(OR), 97202"}
+    {address: "12343 NE Holladay Pl Portland, Oregon(OR), 97230"},
+    {address: "15564 SE Morrison St Portland, Oregon(OR), 97233"},
+    {address: "3125 SW 82nd Ave Portland, Oregon(OR), 97225"},
+    {address: "222 SW Columbia St Portland, Oregon(OR), 97201"},
+    {address: "2020 SW Broadway Dr #APT 7 Portland, Oregon(OR), 97201"},
+    {address: "1849 SE Ankeny St Portland, Oregon(OR), 97214"},
+    {address: "3417 N Michigan Ave Portland, Oregon(OR), 97227"},
+    {address: "1969 NW Johnson St #APT 302 Portland, Oregon(OR), 97209"},
+    {address: "7128 SW Burlingame Ave Portland, Oregon(OR), 97219"},
+    {address: "10339 NE Prescott St #306 Portland, Oregon(OR), 97220"},
+    {address: "10677 NW Lost Park Dr Portland, Oregon(OR), 97229"},
+    {address: "10145 SW Highland Dr Portland, Oregon(OR), 97224"},
+    {address: "15768 SE Powell Blvd #9 Portland, Oregon(OR), 97236"},
+    {address: "16035 SW King Charles Ave Portland, Oregon(OR), 97224"},
+    {address: "15920 NE Fremont St Portland, Oregon(OR), 97230"},
+    {address: "2536 SE Harrison St #9 Portland, Oregon(OR), 97222"},
+    {address: "236 SE 74th Ave Portland, Oregon(OR), 97215"},
+    {address: "12030 SW 97th Pl Portland, Oregon(OR), 97223"},
+    {address: "12614 E Burnside St Portland, Oregon(OR), 97233"},
+    {address: "200 SW Market St #1770 Portland, Oregon(OR), 97201"},
+    {address: "4926 SW 19th Ave Portland, Oregon(OR), 97201"},
+    {address: "5038 SE 30th Ave #32 Portland, Oregon(OR), 97202"}
 ]
 
 app.get('/api/getAddresses', async(req, res) => {
@@ -64,8 +64,31 @@ app.get('/api/getAddresses', async(req, res) => {
 
 //from there, get lat and lon for each address
 
-geocoder.geocode("5038 SE 30th Ave #32 Portland, Oregon(OR), 97202")
-  .then((res)=> {console.log(res[0])})
+let newAddressArray = [];
+function massageAddressArray(array) {
+  array.forEach((address)=> {
+    geocoder.geocode(address.address)
+      .then((res)=>{
+        newAddressArray.push({formattedAddress: res[0].formattedAddress, latitude: res[0].latitude, longitude: res[0].longitude})
+        //console.log(newAddressArray) 
+        const finalAddressArray = newAddressArray.map(addressObj=> {
+        return new MasterAddress(addressObj) 
+        })
+        console.log('finalAddressArray :', finalAddressArray)
+        return finalAddressArray
+      })  
+  })
+}
+
+massageAddressArray(randomAddresses);
+
+class MasterAddress {
+  constructor(address) {
+    this.formattedAddress = address.formattedAddress,
+    this.latitude = address.latitude,
+    this.longitude = address.longitude
+  }
+}
 
   //create algorith using the divide and conquer algorithm to find the x amount of farthest points (based on # of drivers available ) and then 
 //find the closest distance between those other points to each cluster center
